0a599a21631775ffa9fc3945dfde22b9999d27ac
Joseph Kois <josephkois@gmail.com>
2022-04-22 14:02:22 -0700

    prog on instrument

--- client/src/instruments/josephkois.tsx
+++ client/src/instruments/josephkois.tsx
@@ -7,4 +7,12 @@ import React from 'react';
 // project imports
 import { Instrument, InstrumentProps } from '../Instruments';
 
+interface ViolinKeyProps {
+    note: string; 
+    duration?: string;
+    synth?: Tone.Synth; // Contains library code for making sound
+    octave: number;
+    index: number; // octave + index together give a location for the piano key
+}
+
 // export const ViolinInstrument = new Instrument('JosephkoisViolin', Violin);
