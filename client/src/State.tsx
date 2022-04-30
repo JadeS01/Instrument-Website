38a6a7559d5ce2800b954c7e8f70ec033fcfe855
Joseph Kois <josephkois@gmail.com>
2022-04-30 14:44:40 -0700

    small change

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -3,7 +3,8 @@ import { List, Map } from 'immutable';
 
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
-import { HarpInstrument } from './instruments/JadeS01'
+import { ViolinInstrument } from './instruments/josephkois';
+import { HarpInstrument } from './instruments/JadeS01';
 import { FluteInstrument } from './instruments/ViP-Cente';
 import { DrumsInstrument } from './instruments/kayvaunSF';
 import { WaveformVisualizer } from './visualizers/Waveform';
@@ -20,7 +21,7 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument, HarpInstrument, FluteInstrument, DrumsInstrument]);       // similar to Instrument[]
+const instruments = List([PianoInstrument, ViolinInstrument, HarpInstrument, FluteInstrument, DrumsInstrument]);       // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
--- client/src/instruments/josephkois.tsx
+++ client/src/instruments/josephkois.tsx
@@ -6,11 +6,14 @@ import React from 'react';
 
 // project imports
 import { Instrument, InstrumentProps } from '../Instruments';
+import { RecursivePartial } from 'tone/Tone/core/util/Interface';
+import { OmniOscillatorOptions } from 'tone';
 
 interface ViolinStringProps {
-    note: string; 
+    note: string; // C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
     duration?: string;
     synth?: Tone.Synth; // Contains library code for making sound
+    minor?: boolean; // True if minor key, false if major key
     octave: number;
     index: number; // octave + index together give a location for the piano key
 }
@@ -18,6 +21,7 @@ interface ViolinStringProps {
 export function ViolinString({
     note,
     synth,
+    minor,
     index,
 }: ViolinStringProps): JSX.Element {
     /**
@@ -33,54 +37,60 @@ export function ViolinString({
         onMouseDown={() => synth?.triggerAttack(`${note}`)} // Question: what is `onMouseDown`?
         onMouseUp={() => synth?.triggerRelease('+0.25')} // Question: what is `onMouseUp`?
         className={classNames('ba pointer absolute dim', {
-          //'bg-black black h3': minor, // minor keys are black
-          //'black bg-white h4': !minor, // major keys are white
+          'bg-black black h3': minor, // minor keys are black
+          'black bg-white h4': !minor, // major keys are white
         })}
         style={{
-        // CSS
-        top: 0,
-        left: `${index * 2}rem`,
-        zIndex: 1,
-        width: '1.5rem',
-        marginLeft: '0.25rem',
+            // CSS
+            top: 0,
+            left: `${index * 2}rem`,
+            zIndex: minor ? 1 : 0,
+            width: minor ? '1.5rem' : '2rem',
+            marginLeft: minor ? '0.25rem' : 0,
         }}
     ></div>
     );
 }
 
   // eslint-disable-next-line
-// function ViolinStringWithoutJSX({
-//     note,
-//     synth,
-//     index,
-// }: ViolinStringProps): JSX.Element {
-//     /**
-//      * This React component for pedagogical purposes.
-//      * See `ViolinKey` for the React component with JSX (JavaScript XML).
-//      */
-//     return React.createElement(
-//     'div',
-//     {
-//         onMouseDown: () => synth?.triggerAttack(`${note}`),
-//         onMouseUp: () => synth?.triggerRelease('+0.25'),
-//         className: classNames('ba pointer absolute dim', {
-//         }),
-//         style: {
-//         top: 0,
-//         left: `${index * 2}rem`,
-//         },
-//     },
-//     [],
-//     );
-// }
+function ViolinStringWithoutJSX({
+    note,
+    synth,
+    minor,
+    index,
+}: ViolinStringProps): JSX.Element {
+    /**
+     * This React component for pedagogical purposes.
+     * See `ViolinString` for the React component with JSX (JavaScript XML).
+     */
+    return React.createElement(
+    'div',
+    {
+        onMouseDown: () => synth?.triggerAttack(`${note}`),
+        onMouseUp: () => synth?.triggerRelease('+0.25'),
+        className: classNames('ba pointer absolute dim', {
+            'bg-black black h3': minor,
+            'black bg-white h4': !minor,
+        }),
+        style: {
+            top: 0,
+            left: `${index * 2}rem`,
+            zIndex: minor ? 1 : 0,
+            width: minor ? '1.5rem' : '2rem',
+            marginLeft: minor ? '0.25rem' : 0,
+        },
+    },
+    [],
+    );
+}
 
 function ViolinType({ title, onClick, active }: any): JSX.Element {
     return (
     <div
         onClick={onClick}
         className={classNames('dim pointer ph2 pv1 ba mr2 br1 fw7 bw1', {
-        'b--black black': active,
-        'gray b--light-gray': !active,
+            'b--black black': active,
+            'gray b--light-gray': !active,
         })}
     >
         {title}
@@ -89,19 +99,19 @@ function ViolinType({ title, onClick, active }: any): JSX.Element {
 }
 
 function Violin({ synth, setSynth }: InstrumentProps): JSX.Element {
-    const strings = List([
-    { note: 'C', idx: 0 },
-    { note: 'Db', idx: 0.5 },
-    { note: 'D', idx: 1 },
-    { note: 'Eb', idx: 1.5 },
-    { note: 'E', idx: 2 },
-    { note: 'F', idx: 3 },
-    { note: 'Gb', idx: 3.5 },
-    { note: 'G', idx: 4 },
-    { note: 'Ab', idx: 4.5 },
-    { note: 'A', idx: 5 },
-    { note: 'Bb', idx: 5.5 },
-    { note: 'B', idx: 6 },
+    const keys = List([
+        { note: 'C', idx: 0 },
+        { note: 'Db', idx: 0.5 },
+        { note: 'D', idx: 1 },
+        { note: 'Eb', idx: 1.5 },
+        { note: 'E', idx: 2 },
+        { note: 'F', idx: 3 },
+        { note: 'Gb', idx: 3.5 },
+        { note: 'G', idx: 4 },
+        { note: 'Ab', idx: 4.5 },
+        { note: 'A', idx: 5 },
+        { note: 'Bb', idx: 5.5 },
+        { note: 'B', idx: 6 },
     ]);
 
     const setOscillator = (newType: Tone.ToneOscillatorType) => {
@@ -109,49 +119,51 @@ function Violin({ synth, setSynth }: InstrumentProps): JSX.Element {
         oldSynth.disconnect();
 
         return new Tone.Synth({
-        oscillator: { type: newType } as Tone.OmniOscillatorOptions,
+            oscillator: { type: newType } as Tone.OmniOscillatorOptions,
         }).toDestination();
     });
     };
 
     const oscillators: List<OscillatorType> = List([
-    'sine',
-    'sawtooth',
-    'square',
-    'triangle',
-    'fmsine',
-    'fmsawtooth',
-    'fmtriangle',
-    'amsine',
-    'amsawtooth',
-    'amtriangle',
+        'sine',
+        'sawtooth',
+        'square',
+        'triangle',
+        'fmsine',
+        'fmsawtooth',
+        'fmtriangle',
+        'amsine',
+        'amsawtooth',
+        'amtriangle',
     ]) as List<OscillatorType>;
 
     return (
     <div className="pv4">
         <div className="relative dib h4 w-100 ml4">
-        {Range(2, 7).map(octave =>
-            strings.map(strings => {
-            const note = `${strings.note}${octave}`;
+            {Range(2, 7).map(octave =>
+            keys.map(key => {
+            const isMinor = key.note.indexOf('b') !== -1;
+            const note = `${key.note}${octave}`;
             return (
                 <ViolinString
-                key={note} //react key
-                note={note}
-                synth={synth}
-                octave={octave}
-                index={(octave - 2) * 7 + strings.idx}
+                    key={note} //react key
+                    note={note}
+                    synth={synth}
+                    minor={isMinor}
+                    octave={octave}
+                    index={(octave - 2) * 7 + key.idx}
                 />
             );
             }),
         )}
         </div>
         <div className={'pl4 pt4 flex'}>
-        {oscillators.map(o => (
+            {oscillators.map(o => (
             <ViolinType
-            key={o}
-            title={o}
-            onClick={() => setOscillator(o)}
-            active={synth?.oscillator.type === o}
+                string={o}
+                title={o}
+                onClick={() => setOscillator(o)}
+                active={synth?.oscillator.type === o}
             />
         ))}
         </div>
